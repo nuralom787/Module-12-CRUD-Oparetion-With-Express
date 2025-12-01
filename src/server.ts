072e0598dc3ed1f2b9744f82de13270a8f8ce6e2
nuralom787 <md9577630@gmail.com>
2025-12-01 20:09:55 +0600

    create crud oparetions

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
@@ -45,14 +45,19 @@ const initDB = async () => {
         `);
 };
 
-initDB()
+initDB();
+
+// Logger Middleware.
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method}`);
+};
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Node-express!')
-});
+
+// * -----------------  Users Related APIS ------------------ *//
 
 
+// ! users CRUD.
 app.post("/users", async (req: Request, res: Response) => {
     const { name, email } = req.body;
 
@@ -72,12 +77,198 @@ app.post("/users", async (req: Request, res: Response) => {
             success: false
         })
     }
-
-
-    res.status(201).json({ message: "ok", success: true });
+    // res.status(201).json({ message: "ok", success: true });
 });
 
 
+// ! Get All users.
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users
+            `);
+        res.status(200).json({
+            success: true,
+            message: "Users retrieved Successfully!",
+            data: result.rows
+        })
+    }
+    catch (err: any) {
+        // console.log(err);    
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        });
+    }
+});
+
+
+// ! Get Single Users.
+app.get("/users/:id", async (req: Request, res: Response) => {
+    // console.log(req.params);
+
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "user not found!"
+            })
+        }
+        else {
+            res.status(200).json({
+                success: false,
+                message: "user Founded",
+                data: result.rows
+            })
+        }
+    }
+    catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        });
+    }
+});
+
+
+// ! Update User Route.
+app.put("/users/:id", async (req: Request, res: Response) => {
+    // console.log(req.params);
+    const { name, email } = req.body;
+
+    try {
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`, [name, email, req.params.id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "user not found!"
+            })
+        }
+        else {
+            res.status(200).json({
+                success: false,
+                message: "user Updated Successfully",
+                data: result.rows
+            })
+        }
+    }
+    catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        });
+    }
+});
+
+
+// ! Delete User.
+app.delete("/users/:id", async (req: Request, res: Response) => {
+    // console.log(req.params);
+
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id]);
+
+        if (result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: "user not found!"
+            })
+        }
+        else {
+            res.status(200).json({
+                success: false,
+                message: "user Deleted Successfully.",
+                data: result.rows
+            })
+        }
+    }
+    catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        });
+    }
+});
+
+
+
+// * --------------------- todos Related APIS ----------------------- * //
+
+
+app.post("/todos", async (req: Request, res: Response) => {
+    const { user_id, title } = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO todos(user_id,title) VALUES($1, $2) RETURNING *`, [user_id, title]);
+
+        res.status(200).json({
+            success: true,
+            message: "Todo Created.",
+            data: result.rows[0]
+        })
+    }
+    catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+
+// ! Get All todos.
+app.get("/todos", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`
+            SELECT * FROM todos
+            `);
+        res.status(200).json({
+            success: true,
+            message: "Todos retrieved Successfully!",
+            data: result.rows
+        })
+    }
+    catch (err: any) {
+        // console.log(err);    
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        });
+    }
+});
+
+
+
+
+// ! ----------------------- NOT FOUND ---------------------- ! //
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: "Route Not Found!",
+        path: req.path
+    })
+})
+
+
+
+// * Default Apis.
+
+// ! Default get Route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Node-express!')
+});
+
+
+// !  listening route.
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 });
